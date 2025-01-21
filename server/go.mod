0f3acaa881126a61fa8d6ac21a6cb04d9fc85527
loicglanoisatynov <loic.glanois@ynov.com>
2025-01-21 11:35:07 +0100

    Feat : first commit

---
+++ server/go.mod
@@ -0,0 +1,3 @@
+module socengai-server
+
+go 1.21.1
---
+++ server/routes/routes.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"net/http"
+)
+
+func Get(h http.HandlerFunc) http.HandlerFunc {
+	return allowMethod(h, "GET")
+}
+
+func allowMethod(h http.HandlerFunc, method string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if method != r.Method {
+			w.Header().Set("Allow", method)
+			http.Error(w, "405 method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		h(w, r)
+	}
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the home page!"))
+}
