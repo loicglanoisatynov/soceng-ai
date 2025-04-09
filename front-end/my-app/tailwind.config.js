c6acbe492fe6d77c41a4301cbebfda5177a8899e
Lykkss <quaglierilisa3@gmail.com>
2025-04-09 12:11:13 +0200

    Feat : refactor application structure by removing unused components and adding About, Auth, Challenge, and Dashboard modules

--- front-end/my-app/tailwind.config.js
+++ front-end/my-app/tailwind.config.js
@@ -1,12 +1,30 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{html,ts}"
-    ],
-    theme: {
-      extend: {},
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5C00BF',
+        secondary: '#FF8600',
+        lightblue: '#AEB8FE',
+        dark: '#2E2E2E',
+        accent: 'rgba(255, 134, 0, 0.38)',
+        white: '#FFFFFF',
+        black: '#000000',
+      },
+      fontFamily: {
+        ocr: ['"OCR A Std Regular"', 'monospace'],
+        press: ['"Press Start 2P"', 'cursive'],
+      },
+      fontSize: {
+        'title-lg': '80px',
+        'title-md': '30px',
+        'text-lg': '20px',
+        'text-sm': '14px',
+      },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [],
+}
