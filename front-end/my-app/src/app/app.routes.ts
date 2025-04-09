441d90bbc8de4488d827ac0ad501237202168168
Lykkss <quaglierilisa3@gmail.com>
2025-04-09 12:30:28 +0200

    Feat : implement routing and add header and footer components with basic structure

--- front-end/my-app/src/app/app.routes.ts
+++ front-end/my-app/src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./auth/login/login.component').then(m => m.LoginComponent)
+      },
+      {
+        path: 'signup',
+        loadComponent: () => import('./auth/signup/signup.component').then(m => m.SignupComponent)
+      }
+    ]
+  },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'challenge',
+    loadComponent: () => import('./challenge/challenge/challenge.component').then(m => m.ChallengeComponent)
+  },
+  { path: '**', redirectTo: 'home' }
+];
