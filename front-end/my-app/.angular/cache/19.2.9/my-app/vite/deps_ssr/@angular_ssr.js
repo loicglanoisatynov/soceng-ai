68f2a26d78075c8d758baf81d9ff10960ce7804d
loicglanois <loicglanois@protonmail.com>
2025-04-29 16:20:36 +0200

    Fix : cached data pushed

---
+++ front-end/my-app/.angular/cache/19.2.9/my-app/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,42 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell
+} from "./chunk-5K7HEQQY.js";
+import "./chunk-CIC47LUM.js";
+import "./chunk-EKF23KUT.js";
+import "./chunk-TFWAX2QS.js";
+import "./chunk-VRBIFGFV.js";
+import "./chunk-23775W4T.js";
+import "./chunk-D4G2ZICO.js";
+import "./chunk-B5CYHLY7.js";
+import "./chunk-XOB36AR6.js";
+import "./chunk-YHCV7DAQ.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRoutesConfig,
+  provideServerRouting,
+  withAppShell,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
