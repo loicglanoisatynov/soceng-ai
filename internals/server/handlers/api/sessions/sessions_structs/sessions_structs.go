e4206d3b144519317a4cdee452f2b64accaf57a3
Lykkss <quaglierilisa3@gmail.com>
2025-05-26 10:37:26 +0200

    Revert "Feat : post session data route"
    This reverts commit 69f54ff615413e251db54b61f7cc893ca35998d2.

--- internals/server/handlers/api/sessions/sessions_structs/sessions_structs.go
+++ internals/server/handlers/api/sessions/sessions_structs/sessions_structs.go
@@ -37,8 +37,3 @@ type Chall_message struct {
 	Timestamp         string `json:"timestamp"`
 	Holds_hint        bool   `json:"holds_hint"`
 }
-
-type Post_session_data_request struct {
-	Character_name string `json:"character_name"`
-	Message        string `json:"message"`
-}
